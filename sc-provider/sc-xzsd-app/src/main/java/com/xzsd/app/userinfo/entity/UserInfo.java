095547023c45dd9baeaa99af441a8998c6167b35
zhaorujie666 <781392805@qq.com>
2020-04-12 10:58:57 +0800

    app端个人信息

--- sc-provider/sc-xzsd-app/src/main/java/com/xzsd/app/userinfo/entity/UserInfo.java
+++ sc-provider/sc-xzsd-app/src/main/java/com/xzsd/app/userinfo/entity/UserInfo.java
@@ -57,7 +57,7 @@ public class UserInfo {
     /**
      * 版本号
      */
-    private int version;
+    private String version;
     /**
      * 原密码
      */
@@ -163,11 +163,11 @@ public class UserInfo {
         this.phone = phone;
     }
 
-    public int getVersion() {
+    public String getVersion() {
         return version;
     }
 
-    public void setVersion(int version) {
+    public void setVersion(String version) {
         this.version = version;
     }
 
