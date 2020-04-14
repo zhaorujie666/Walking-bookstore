7b0459f48af235d891db335fe9a6fc9a54a40581
zhaorujie666 <781392805@qq.com>
2020-04-14 19:21:58 +0800

    店长的个人信息完成

---
+++ sc-provider/sc-xzsd-app/src/main/java/com/xzsd/app/managerinfo/controller/ManagerInfoController.java
@@ -0,0 +1,42 @@
+package com.xzsd.app.managerinfo.controller;
+
+import com.neusoft.core.restful.AppResponse;
+import com.neusoft.security.client.utils.SecurityUtils;
+import com.xzsd.app.managerinfo.service.ManagerInfoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @Description 查询给门店送货的所有司机
+ * @author zhaorujie
+ * @date 2020/4/14
+ */
+@RestController
+@RequestMapping("manangerInformation")
+public class ManagerInfoController {
+    public static final Logger logger = LoggerFactory.getLogger(ManagerInfoController.class);
+
+    @Resource
+    private ManagerInfoService managerInfoService;
+
+    /**
+     * 查询司机信息列表
+     * @return
+     */
+    @PostMapping("listManangerDrivers")
+    public AppResponse getListDriver(){
+        try {
+            String userId = SecurityUtils.getCurrentUserId();
+            return managerInfoService.getListDriver("2020040919381184560");
+        }catch (Exception e){
+            logger.error("查询司机信息列表失败");
+            System.out.println(e.toString());
+            throw e;
+        }
+    }
+}
